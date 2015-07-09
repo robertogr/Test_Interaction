a32ffeeefcf8360edc2d4c68e3534f8fb4399b55
unknown <roberto.gonzalez@Thinkpad-1127.fundacionctic.org>
2015-07-09 11:58:07 +0200

    Added GUICrossHair.cs but do nothing yet

---
+++ Assets/GUICrossHair.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUICrossHair : MonoBehaviour
+{
+
+	public Texture2D CrossHair;
+	public Rect position;
+	public 
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
